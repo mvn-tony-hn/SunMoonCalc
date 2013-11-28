663090d347d938f4426194a712d5e2af118d6b16
LongPD <longpd90@gmail.com>
2013-11-28 11:40:52 +0700

    refactor code

--- SunMoonCalcGobal.m
+++ SunMoonCalcGobal.m
@@ -41,7 +41,6 @@ BOOL SunSet = NO;
 
 - (id)init
 {
-    
     self = [super init];
     if (self) {
         _dateFormatter = [[NSDateFormatter alloc] init];
@@ -50,150 +49,105 @@ BOOL SunSet = NO;
         _dateFormatterConvert = [[NSDateFormatter alloc] init];
         positionEntity = [[PositionEntity alloc]init];
     }
-    
     return self;
-    
 }
 
 #pragma mark - Get Moon , Sun Position Global
 
 - (double)getSolarMeanAnomalyWithDayNumber:(double)d {
-    
     return DR * (357.5291 + 0.98560028 * d);
-    
 }
 
 - (double)getEquationOfCenterWithSolarMean:(double)M {
-    
     return DR * (1.9148 * sin(M) + 0.02 * sin(2*M) + 0.0003 * sin(3*M));
-    
 }
 
 - (double)getEclipticLongitudeWithSolarMean:(double)M andCenter:(double)C {
-    
     double P = DR * 102.9372; // điểm cận nhật của trái đất.
     return M + C + P + M_PI;
-    
 }
 
 - (double)e{
-    
     return DR * 23.4397;//obliquity(do nghieng) of the Earth
-    
 }
 
 
 - (double)getDeclinationWithLongitude:(double)l andLatitude:(double)b {
-    
     return asin(sin(b) * cos(self.e) + cos(b) * sin(self.e) *sin(l));
-    
 }
 
 - (double)getRightAscensionWithLongitude:(double)l andLatitude:(double)b {
-    
     return atan2(sin(l) * cos(self.e) - tan(b) * sin(self.e), cos(l));
-    
 }
 
 - (double)secondInDay {
-    
     return 60*60*24 ;
-    
 }
 
 - (double)J1970 {
-    
     return 2440588;
-    
 }
 
 - (double)J2000 {
-    
     return 2451545;
-    
 }
 
 - (void)toJulianDate
 {
-    
     NSString *JulianString = @"1970-01-01 00:00:00";
     _julianDate = [_dateFormatter dateFromString:JulianString];
-    
 }
 
 - (double) toJulian:(NSDate*)date {
-    
     if (!_julianDate) {
         [self toJulianDate];
     }
     NSTimeInterval differentBetweenDates = [date timeIntervalSinceDate:_julianDate];
     return differentBetweenDates/ self.secondInDay - 0.5 + self.J1970;
-    
 }
 
 - (double)toDays:(NSDate*)date {
-    
     return [self toJulian:date] - self.J2000;
-    
 }
 
 - (double)getSiderealTimeWithDayNumber:(double)d andObserverLongitude:(double)lw{
-    
     return DR * (280.16 + 360.9856235 * d) - lw;
-    
 }
 
 - (double)getAzimuthWithHourAngle:(double)H observerLatitude:(double)phi andDeclination:(double)dec {
-    
     return M_PI + atan2(sin(H), cos(H)*sin(phi) - tan(dec) * cos(phi));
-    
 }
 
 - (double)getAltitudeWithHourAngle:(double)H observerLatitude:(double)phi andDeclination:(double)dec {
-    
     return asin(sin(phi)*sin(dec) + cos(phi)*cos(dec)*cos(H));
-    
 }
 
 - (double)J0 {
-    
     return 0.0009;
-    
 }
 
 - (NSDate *)fromJulian:(double)j {
-    
     double timeInterval = (j+ 0.5 - self.J1970) * [self secondInDay];
     NSDate * date = [[NSDate alloc]initWithTimeIntervalSince1970:timeInterval];
     return date;
-    
 }
 
 - (double)getJulianCycleWithDayNumber:(double)d andObserverLongitude:(double)lw {
-    
     return round(d-self.J0-lw/(2*M_PI));
-    
 }
 
 - (double)getApproxTransitWithHourAngle:(double)Ht andObserverLongitude:(double)lw andJulianCycle:(double)n {
-    
     return self.J0 + (Ht + lw)/(2*M_PI) + n;
-    
 }
 - (double)getSolarTransitJWithApproxTransit:(double)ds andSolarMeanTime:(double)M andEclipticLongitude:(double)L {
-    
     return self.J2000 + ds + 0.0053*sin(M) - 0.0069 * sin(2*L);
-    
 }
 
 - (double)getHourAngleWithAltitude:(double)h andObserverLatitude:(double)phi andDeclination:(double)d {
-    
     return acos((sin(h) - sin(phi)*sin(d))/(cos(phi) *cos(d)));
-    
 }
 
 - (NSMutableArray*)times {
-    
     NSMutableArray *times = [[NSMutableArray alloc]init ];
     NSArray *temp;
     temp = [[NSArray alloc]initWithObjects: @"-0.83",@"sunrise",@"sunset", nil ];
@@ -209,7 +163,6 @@ BOOL SunSet = NO;
     temp = [[NSArray alloc]initWithObjects:@"6",@"goldenHourEnd",@"goldenHour", nil];
     [times addObject:temp];
     return times;
-    
 }
 
 - (CGPoint )setPoint:(BOOL)isHiddenPoint andAzimuth:(double)azimuth andAltitude:(double)altitude
@@ -227,7 +180,6 @@ BOOL SunSet = NO;
 }
 
 # pragma mark - get Sun Time
-
 - (NSDictionary *)getSunTimesWithDate:(NSDate*)date andLatitude:(double)lat andLogitude:(double)lng {
     
     double lw = DR * -lng;
@@ -259,9 +211,7 @@ BOOL SunSet = NO;
         [sunTimes setValue:[self fromJulian:Jset]  forKey:[NSString stringWithFormat:@"%@",[time objectAtIndex:2]]];
         
     }
-    
     return sunTimes;
-    
 }
 
 #pragma mark - get Moon Fraction
@@ -274,7 +224,6 @@ BOOL SunSet = NO;
     double sdist = 149598000.0;// distance from Earth to Sun in km
     double phi = acos(sin(sunCoordinate.declination) * sin(moonCoordinate.declination) + cos(sunCoordinate.declination) * cos(moonCoordinate.declination) * cos(sunCoordinate.rightAscension - moonCoordinate.rightAscension));
     double inc = atan2(sdist * sin(phi), moonCoordinate.distance - sdist * cos(phi));
-    
     return (1 + cos(inc)) / 2.0f;
 }
 
@@ -282,26 +231,21 @@ BOOL SunSet = NO;
 
 - (void)setSun:(NSDate *)date withLatitude:(double)lat withLongitude:(double)lng
 {
-    
     [self getSunPositionWithDate:date andLatitude:lat andLongitude:lng];
     positionEntity.pointSunRise = [self setPoint:!MoonRise andAzimuth:Rise_azS*DR andAltitude:0];
     positionEntity.pointSunSet = [self setPoint:!MoonSet andAzimuth:Set_azS*DR andAltitude:0];
-    
 }
 
 - (SunCoordinate*)getSunCoordsWithDayNumber:(double)julianDay {
-    
     double M = [self getSolarMeanAnomalyWithDayNumber:julianDay];
     double C = [self getEquationOfCenterWithSolarMean:M];
     double L = [self getEclipticLongitudeWithSolarMean:M andCenter:C];
     SunCoordinate *sunCoordinate = [[SunCoordinate alloc] initWithDeclination:[self getDeclinationWithLongitude:L andLatitude:0]
                                                   andRightAscension:[self getRightAscensionWithLongitude:L andLatitude:0]];
     return sunCoordinate;
-    
 }
 
 - (SunPosition *)getSunPositionWithDate:(NSDate*)date andLatitude:(double)lat andLongitude:(double)lng {
-    
     double lw = DR * (-lng);
     double phi = DR * lat;
     double d = [self toDays:date];
@@ -314,7 +258,6 @@ BOOL SunSet = NO;
                                                 andAltitude:[self getAltitudeWithHourAngle:H observerLatitude:phi andDeclination:c.declination]];
     [self setSunPositionWithTime:sunPosition withDate:date];
     return sunPosition;
-
 }
 
 - (void)setSunPositionWithTime:(SunPosition *)sunPostion withDate:(NSDate *)date
@@ -336,25 +279,19 @@ BOOL SunSet = NO;
         sunPositionHidden = (date.timeIntervalSince1970 < timeRiseSun.timeIntervalSince1970 || date.timeIntervalSince1970 > timeSetSun.timeIntervalSince1970);
     }
     positionEntity.pointSun = [self setPoint:sunPositionHidden andAzimuth:sunPostion.azimuth andAltitude:sunPostion.altitude];
-
 }
-
 
 #pragma mark - set moon position 
 
 - (void)setMoon:(NSDate *)date withLatitude:(double)lat withLongitude:(double)lng
 {
-    
     MoonPosition *moonPosition = [self getMoonPositionWithDate:date andLatitude:lat andLongitude:lng ];
     [self setMoonPositionWithTime:moonPosition withDate:date];
-    
     positionEntity.pointMoonRise = [self setPoint:!MoonRise andAzimuth:Rise_azM*DR andAltitude:0];
     positionEntity.pointMoonSet = [self setPoint:!MoonSet andAzimuth:Set_azM*DR andAltitude:0];
-    
 }
 
 - (MoonCoordinate *)getMoonCoords:(double)julianday { // geocentric ecliptic coordinates of the moon
-    
     double L = DR * (218.316 + 13.176396 * julianday), // ecliptic longitude
     M = DR * (134.963 + 13.064993 * julianday), // mean anomaly
     F = DR * (93.272 + 13.229350 * julianday), // mean distance
@@ -366,11 +303,9 @@ BOOL SunSet = NO;
                                                          andRightAscension:[self getRightAscensionWithLongitude:l andLatitude:b]
                                                                andDistance:dt];
     return moonCoordinate;
-    
 }
 
 - (MoonPosition *)getMoonPositionWithDate:(NSDate*)date andLatitude:(double)lat andLongitude:(double)lng {
-
     double lw = DR * -lng;
     double phi = DR * lat;
     double d = [self toDays:date];
@@ -385,15 +320,11 @@ BOOL SunSet = NO;
                                                       andAltitude:h
                                                       andDistance:c.distance];
     return moonPosition;
-    
 }
-
 
 - (void)setMoonPositionWithTime:(MoonPosition *)moonPostion withDate:(NSDate *)date
 {
-    
     BOOL moonPositionHidden;
-    
     if ((!MoonRise)&&(!MoonSet))                 // neither sunrise nor sunset
     {
         moonPositionHidden = (VHzM[2] < 0);
@@ -413,7 +344,6 @@ BOOL SunSet = NO;
     }
     _todayHaveMoon = !moonPositionHidden;
     positionEntity.pointMoon = [self setPoint:moonPositionHidden andAzimuth:moonPostion.azimuth andAltitude:moonPostion.altitude];
-    
 }
 
 #pragma mark - compute moonrise and moon set
@@ -446,7 +376,6 @@ BOOL SunSet = NO;
         mp[k][2] = SkyM[2];
         jd = jd + 0.5;
     }
-    
     if (mp[1][0] <= mp[0][0]){
         mp[1][0] = mp[1][0] + 2 * M_PI;
     }
@@ -454,13 +383,11 @@ BOOL SunSet = NO;
     if (mp[2][0] <= mp[1][0]){
         mp[2][0] = mp[2][0] + 2 * M_PI;
     }
-    
     RAnM[0] = mp[0][0];
     DecM[0] = mp[0][1];
     double ph = 0.0;
     MoonRise = NO;
     MoonSet = NO;
-    
     for (int k = 0; k < 24; k++)                   // check each hour of this day
     {
         ph = (k + 1)/24.0;
@@ -471,16 +398,13 @@ BOOL SunSet = NO;
         DecM[0] = DecM[2];
         VHzM[0] = VHzM[2];
     }
-    
     timeRiseMoon = [self getTimesFromHour:Rise_timeM[0] andMinute:Rise_timeM[1] andZone:zone];
     timeSetMoon = [self getTimesFromHour:Set_timeM[0] andMinute:Set_timeM[1] andZone:zone];
     [self setMoon:date withLatitude:lat withLongitude:lng];
-    
 }
 
 - (double)test_moon:(double)k withZone:(double)zone witht0:(double)t0 withLat:(double)lat withPlx:(double)plx
 {
-    
     double ha[3] = {0.0, 0.0, 0.0};
     double a, b, c, d, e, s, z;
     double hr, min, time;
@@ -554,15 +478,11 @@ BOOL SunSet = NO;
         MoonSet = YES;
     }
     return VHzM[2];
-    
 }
-
 
 - (void)moon:(double )jd
 {
-    
     double d, f, g, h, m, n, s, u, v, w;
-    
     h = 0.606434 + 0.03660110129*jd;
     m = 0.374897 + 0.03629164709*jd;
     f = 0.259091 + 0.0367481952 *jd;
@@ -624,20 +544,15 @@ BOOL SunSet = NO;
     s = w/sqrt(u - v*v);
     // compute moon's right ascension ...
     SkyM[0] = h + atan(s/sqrt(1 - s*s));
-    
     s = v/sqrt(u);                        // declination ...
     SkyM[1] = atan(s/sqrt(1 - s*s));
-    
     SkyM[2] = 60.40974 * sqrt( u );          // and parallax
-    
 }
-
 
 #pragma mark - compute Sun rise and Sun set
 
 - (void)computeSunriseAndSunSet:(NSDate *)date withLatitude:(double)lat withLongitude:(double)lng
 {
-    
     Rise_azS = 0.0;
     Set_azS = 0.0;
     Rise_timeS[0] = 0.0;
@@ -683,17 +598,13 @@ BOOL SunSet = NO;
         DecS[0] = DecS[2];
         VHzS[0] = VHzS[2];
     }
-    
     timeRiseSun = [self getTimesFromHour:Rise_timeS[0] andMinute:Rise_timeS[1] andZone:zone];
     timeSetSun = [self getTimesFromHour:Set_timeS[0] andMinute:Set_timeS[1] andZone:zone];
     [self setSun:date withLatitude:lat withLongitude:lng];
-    
 }
-
 
 -(double)test_hour:(double)k withZone:(double)zone wihtT0:(double)t0 withLat:(double)lat
 {
-    
     double ha[3] = {0.0, 0.0, 0.0};
     double a, b, c, d, e, s, z;
     double hr, min, time;
@@ -753,7 +664,6 @@ BOOL SunSet = NO;
         Rise_azS = az;
         Sunrise = YES;
     }
-    
     if ((VHzS[0] > 0)&&(VHzS[2] < 0))
     {
         Set_timeS[0] = hr;
@@ -761,15 +671,11 @@ BOOL SunSet = NO;
         Set_azS = az;
         SunSet = YES;
     }
-    
     return VHzS[2];
-    
 }
-
 
 - (void)sun:(double)jd withCT:(double)ct
 {
-    
     double g, lo, s, u, v, w;
     
     lo = 0.779072 + 0.00273790931*jd;
@@ -801,82 +707,67 @@ BOOL SunSet = NO;
     
     s = v/sqrt(u);                        // ...and declination
     SkyS[1] = atan(s/sqrt(1 - s*s));
-    
 }
 
 #pragma mark - sunmoon global
 
 - (NSDate *)getTimesFromHour:(int)hour andMinute:(int)minute andZone:(double)zone
 {
-    
     NSString *dateStringRise = [NSString stringWithFormat:@"%d-%d-%d %d:%d:00",_yearValue,_monthValue,_dayValue,hour,minute];
     NSDate *dateMoonRise = [_dateFormatter dateFromString:dateStringRise];
     return [NSDate dateWithTimeInterval:(zone)*60*60 sinceDate:dateMoonRise];
-    
 }
 
 - (double )calculateJulianDay:(NSDate *)date withLatitude:(double)lat withLongitude:(double)lng
 {
-    
     double x = lng;
     double zone = round(-x/15.0);
-    
     double timeZoneOffset = [[NSTimeZone systemTimeZone] secondsFromGMTForDate:date]/3600.0;
     NSDate *dateLocation = [NSDate dateWithTimeInterval:-(zone + timeZoneOffset)*60*60 sinceDate:date];
     
     _dayValue  = [[self conVertDateToStringFomatBy:@"dd" withDate:dateLocation]intValue];
     _monthValue = [[self conVertDateToStringFomatBy:@"MM" withDate:dateLocation]intValue];
     _yearValue = [[self conVertDateToStringFomatBy:@"yyyy" withDate:dateLocation] intValue];
-    
     return [self julian_day:_yearValue withMonth:_monthValue withDay:_dayValue] - 2451545.0;
-    
 }
 
 - (int)sgn:( double )x
 {
-    
     int rv;
     if (x > 0.0)      rv =  1;
     else if (x < 0.0) rv = -1;
     else              rv =  0;
     return rv;
-    
 }
+
 - (double)interpolate:(double)f0 withf1:(double)f1 withf2:(double)f2 withp:(double)p
 {
-    
     double a = f1 - f0;
     double b = f2 - f1 - a;
     double f = f0 + p * ( 2*a + b * ( 2 * p - 1));
     return f;
-    
 }
 
 - (double)lst :(double )longitude withJday:(double)jday withZ:(double)z
 {
-    
     double s = 24110.5 + 8640184.812999999 * jday / 36525 + 86636.6 * z + 86400 * longitude;
     s = s/86400.0;
     s = s - floor(s);
     s = s * 360.0 * DR;
     return s;
-    
 }
 
 - (double)julian_day :(int)year withMonth:(int)month withDay:(int)day
 {
-    
     double a, b, jd;
     BOOL gregorian;
     month = month ;
     gregorian = (year < 1583) ? false : true;
-    
     if ((month == 1)||(month == 2))
     {
         year  = year  - 1;
         month = month + 12;
     }
-    
     a = floor(year/100.0) ;
     if (gregorian){
         b = 2 - a + floor(a/4.0);
@@ -885,19 +776,15 @@ BOOL SunSet = NO;
         b = 0.0;
     }
     jd = floor(365.25 * (year + 4716)) + floor(30.6001 * (month + 1)) + day + b - 1524.5;
-    
     return jd;
-    
 }
 
 #pragma mark -  convert Date
 
 - (NSString *)conVertDateToStringFomatBy:(NSString *)formatString withDate:(NSDate *)date
 {
-    
     [_dateFormatterConvert setDateFormat:formatString];
     return [_dateFormatterConvert stringFromDate:date];
-    
 }
 
 @end
